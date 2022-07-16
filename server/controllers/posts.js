3402defd4ace3650f8c76b9467b290a1c560d0cd
kirby ellingson <hkellingson@gmail.com>
2022-07-16 16:19:32 -0500

    added get and post routes with logic @/posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,10 +2,35 @@
 import PostMessage from '../models/postMessage.js';
 // logic for posts routes
 //get.req@/posts
-export const getPosts = (req, res) => {
-	res.send('succesfull place holder GET ROUTE');
+export const getPosts = async (req, res) => {
+	try {
+		// get all post Messages
+		const postMessages = await PostMessage.find();
+		console.log(postMessages);
+
+		res.status(200).json(postMessages);
+	} catch (err) {
+		let errMessage = err.message;
+		res
+			.status(404)
+			.send(
+				`<p>get.req@/posts ERROR MESSAGE HERE --> ${errMessage}</p><img src='https://http.cat/404'></img>"`
+			); //CHANGE THIS BEFORE COMMERCIAL DEPLOYMENT
+	}
 };
 // post.req@/posts
-export const createPost = (req, res) => {
-	res.send('make a post place holder /posts');
+export const createPost = async (req, res) => {
+	const post = req.body;
+	const newPost = new PostMessage(post);
+	try {
+		await newPost.save();
+		res.status(201).json(newPost);
+	} catch (err) {
+		let errMessage = err.message;
+		res
+			.status(400)
+			.send(
+				`<p>post.req@/posts ERROR MESSAGE HERE --> ${errMessage}</p><img src='https://http.cat/400'></img>"`
+			); //CHANGE THIS BEFORE COMMERCIAL DEPLOYMENT
+	}
 };
