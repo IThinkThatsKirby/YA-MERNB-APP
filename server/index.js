99c1786be228243ea7f1920b9889a847ebbb0eea
kirby ellingson <hkellingson@gmail.com>
2022-07-15 21:28:12 -0500

    ServerStarts&&DBconnects&&Client w/BUN trial run !

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// remember to add -- { "type" : module } -- in package.json or import wont work
+import 'dotenv/config'; // fancy es6 WOW :D
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// initialize app express instance
+const app = express();
+// middle ware
+// allow size for some images
+app.use(bodyParser.json({ limit: '25mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '25mb', extended: true }));
+app.use(cors());
+// mongodb connection and server port variables
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
+await mongoose // we get there when we get there
+	.connect(CONNECTION_URL, {
+		// The following is NOT necessary but to be SAFE
+		useNewURLParser: true, //uses NEW and FANCY mongoose URL parser
+		useUnifiedTopology: true, //uses NEW and FANCY mongoose Server Discover and Monitoring engine
+	}) // Message for successful DB connection
+	.then(() => {
+		app.listen(PORT, () =>
+			console.log(
+				`Server is running on ${PORT}, mongoose DB CONNECTION SUCCESSFUL`
+			)
+		);
+	}) // Message for failure ;(
+	.catch((err) => {
+		console.log(`CONNECTION ERROR LOOK HERE -->${err.message}`);
+	});
