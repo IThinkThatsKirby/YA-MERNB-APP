2a8c34a8676d55cdcff5712c27ca7dce46f922f8
kirby ellingson <hkellingson@gmail.com>
2022-07-16 20:29:38 -0500

    fix all the things :D and some sad APP.js buildout

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import bottle from './images/bottle-1694868.jpg';
+const App = () => {
+	return (
+		<Container maxwidth='large'>
+			<AppBar position='static' color='inherit'>
+				<Typography varient='h2' align='center'>
+					Messages
+				</Typography>
+				<img src={bottle} alt='message in a bottle' />
+			</AppBar>
+			<Grow in>
+				<Container>
+					<Grid container justify='space-between' alignItems='stretch'>
+						<Grid items xs={12} sm={7}>
+							{/* <Posts /> */}
+						</Grid>
+						<Grid items xs={12} sm={4}>
+							{/* <Form /> */}
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+		</Container>
+	);
+};
+
+export default App;
